2c848eea0253c58cea00cb9b8e5409c2ca3e074a
epson121 <traxdater12@gmail.com>
2014-02-13 17:17:44 +0100

    Templates, frontend, controllers  Creating controller, block, assemble page

---
+++ app/code/local/Test/Module/controllers/RenderController.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of RenderController
+ *
+ * @author luka
+ */
+class Test_Module_RenderController extends Mage_Core_Controller_Front_Action {
+    
+    public function blockAction() {
+        $this->getResponse()->setBody("Hello world");
+    }
+    
+    public function overrideAction() {
+        $blockHtml = $this->getLayout()->createBlock('test_module/sample')->toHtml();
+        $this->getResponse()->setBody($blockHtml);
+    }
+    
+    public function templateAction() {
+        // create block -> mage/core/Block/Template.php
+        // setTemplate -> relative to templates folder (app/design/default/base/Template)
+        $blockHtml = $this->getLayout()
+                                   ->createBlock('core/template')
+                                   ->setTemplate('test_module/random.phtml')
+                                   ->toHtml();
+        $this->getResponse()->setBody($blockHtml);
+    }
+}
