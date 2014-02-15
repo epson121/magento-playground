e71a1a25e592f2d191ad305623ecacbaed8e1402
epson121 <traxdater12@gmail.com>
2014-02-15 23:59:25 +0100

    Update

--- app/code/local/Training/Animal/sql/training_animal_setup/mysql4-install-0.1.1.php
+++ app/code/local/Training/Animal/sql/training_animal_setup/mysql4-install-0.1.1.php
@@ -10,10 +10,7 @@ $installer->run("
 		entity_id INT(11) Unsigned auto_increment primary key,
 		name varchar(255) not null default '',
 		type varchar(255) not null default '',
-		edible tinyint(1) unsigned not null default 1,
-		comment text null,
-		updated_at datetime,
-		created_at datetime
+		edible tinyint(1) unsigned not null default 1
 	) Engine=InnoDB DEFAULT CHARSET=utf8;
 	");
 
